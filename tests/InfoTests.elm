40d16e224c07b922b4695d35e4cb54908ed11238
katjam <katja@neontribe.co.uk>
2019-08-01 21:16:20 +0100

    Translate info in html & pass info tests on keys

--- tests/InfoTests.elm
+++ tests/InfoTests.elm
@@ -1,6 +1,7 @@
 module InfoTests exposing (all)
 
 import Expect
+import I18n.Keys exposing (Key(..))
 import Info exposing (getInfo)
 import Test exposing (Test, describe, test, todo)
 
@@ -11,67 +12,67 @@ all =
         [ describe "Info name"
             [ test "Index of 1" <|
                 \() ->
-                    (getInfo 1).name |> Expect.equal "About the project"
+                    (getInfo 1).name |> Expect.equal InfoOneName
             , test "Index of 2" <|
                 \() ->
-                    (getInfo 2).name |> Expect.equal "Bite size information"
+                    (getInfo 2).name |> Expect.equal InfoTwoName
             , test "Index of 3" <|
                 \() ->
-                    (getInfo 3).name |> Expect.equal "Visual Storytelling"
+                    (getInfo 3).name |> Expect.equal InfoThreeName
             , test "Index of 4" <|
                 \() ->
-                    (getInfo 4).name |> Expect.equal "What is the survey?"
+                    (getInfo 4).name |> Expect.equal InfoFourName
             , test "Index of 5" <|
                 \() ->
-                    (getInfo 5).name |> Expect.equal "Can my organisation try this?"
+                    (getInfo 5).name |> Expect.equal InfoFiveName
             , test "Index of 500" <|
                 \() ->
-                    (getInfo 500).name |> Expect.equal "Not found"
+                    (getInfo 500).name |> Expect.equal InfoNotFoundName
             ]
         , describe "Info slug"
             [ test "Index of 1" <|
                 \() ->
-                    (getInfo 1).slug |> Expect.equal "about"
+                    (getInfo 1).slug |> Expect.equal InfoOneSlug
             , test "Index of 2" <|
                 \() ->
-                    (getInfo 2).slug |> Expect.equal "bite-size-info"
+                    (getInfo 2).slug |> Expect.equal InfoTwoSlug
             , test "Index of 3" <|
                 \() ->
-                    (getInfo 3).slug |> Expect.equal "visual-storytelling"
+                    (getInfo 3).slug |> Expect.equal InfoThreeSlug
             , test "Index of 4" <|
                 \() ->
-                    (getInfo 4).slug |> Expect.equal "survey"
+                    (getInfo 4).slug |> Expect.equal InfoFourSlug
             , test "Index of 5" <|
                 \() ->
-                    (getInfo 5).slug |> Expect.equal "try-this"
+                    (getInfo 5).slug |> Expect.equal InfoFiveSlug
             , test "Index of 0" <|
                 \() ->
-                    (getInfo 0).slug |> Expect.equal "not-found"
+                    (getInfo 0).slug |> Expect.equal InfoNotFoundSlug
             , test "Index of 500" <|
                 \() ->
-                    (getInfo 500).slug |> Expect.equal "not-found"
+                    (getInfo 500).slug |> Expect.equal InfoNotFoundSlug
             ]
         , describe "Info icon"
             [ test "Index of 1" <|
                 \() ->
-                    (getInfo 1).icon |> Expect.equal "question-circle-o"
+                    (getInfo 1).icon |> Expect.equal InfoOneIcon
             , test "Index of 2" <|
                 \() ->
-                    (getInfo 2).icon |> Expect.equal "info-circle"
+                    (getInfo 2).icon |> Expect.equal InfoTwoIcon
             , test "Index of 3" <|
                 \() ->
-                    (getInfo 3).icon |> Expect.equal "pencil"
+                    (getInfo 3).icon |> Expect.equal InfoThreeIcon
             , test "Index of 4" <|
                 \() ->
-                    (getInfo 4).icon |> Expect.equal "check-square-o"
+                    (getInfo 4).icon |> Expect.equal InfoFourIcon
             , test "Index of 5" <|
                 \() ->
-                    (getInfo 5).icon |> Expect.equal "group"
+                    (getInfo 5).icon |> Expect.equal InfoFiveIcon
             , test "Index of 0" <|
                 \() ->
-                    (getInfo 0).icon |> Expect.equal "question"
+                    (getInfo 0).icon |> Expect.equal InfoNotFoundIcon
             , test "Index of 500" <|
                 \() ->
-                    (getInfo 500).icon |> Expect.equal "question"
+                    (getInfo 500).icon |> Expect.equal InfoNotFoundIcon
             ]
         ]
