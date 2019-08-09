a30c7dd7d2a5eb35501b6f358f65a070dc3d772a
RabidGamer <minihamster@btinternet.com>
2019-08-09 15:52:52 +0100

    Footer only appears on pages we want it on

--- src/View.elm
+++ src/View.elm
@@ -1,11 +1,12 @@
 module View exposing (view)
 
 import Browser
-import Html exposing (div, main_)
+import Html exposing (Html, div, main_, text)
 import Html.Attributes exposing (class, id)
 import Html.Attributes.Aria exposing (role)
-import Messages exposing (Msg)
+import Messages exposing (Msg(..))
 import Model exposing (Model, pageSlug)
+import Route exposing (Page(..))
 import Views.Content
 import Views.Footer
 import Views.Header
@@ -21,7 +22,23 @@ view model =
             , Views.Nav.view model
             , main_ [ class "content", role "main" ]
                 [ Views.Content.view model ]
-            , Views.Footer.footerContent
             ]
+        , renderFooter model.currentPage
         ]
     }
+
+
+renderFooter : Page -> Html Msg
+renderFooter page =
+    case page of
+        Home ->
+            Views.Footer.footerContent
+
+        SupportersPage ->
+            Views.Footer.footerContent
+
+        PrivacyPage ->
+            Views.Footer.footerContent
+
+        _ ->
+            text " "
